e9c6787e5f943e2d568d133c2fe10957dfc01433
Alexei Kopendakov <kopendakov_a@itrail.by>
2023-01-27 21:53:20 +0300

    initial commit

---
+++ src/main/java/net/kopendakov/smallwiki/SmallwikiApplication.java
@@ -0,0 +1,13 @@
+package net.kopendakov.smallwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallwikiApplication.class, args);
+	}
+
+}
